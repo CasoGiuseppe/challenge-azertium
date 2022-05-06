4dc4a0d3f843e07db6dbd7d2d415addde49b14ea
gcaso <76.giuseppe.caso@gmail.com>
2022-05-06 11:35:14 +0200

    add deleted route

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -2,7 +2,10 @@ import { createRouter, createWebHistory } from "vue-router";
 import { galleryServices } from "@/gallery/domain/services/Gallery.services";
 
 import { useGalleryStore } from "@/gallery/stores/gallery";
-import { CHANGE_GALLERY_LIST } from "@/gallery/stores/gallery/actions";
+import {
+  CHANGE_GALLERY_LIST,
+  CHANGE_GALLERY_DELETED_STATE
+} from "@/gallery/stores/gallery/actions";
 import { GET_GALLERY_ALBUM } from "@/gallery/stores/gallery/getters";
 
 const router = createRouter({
@@ -25,7 +28,22 @@ const router = createRouter({
       
         next();
       },
-    },
+
+      children: [
+        {
+          path: "/deleted",
+          name: "deleted",
+          component: {},
+
+          beforeEnter: async (to, from, next) => {
+            const galleryStore = useGalleryStore();
+
+            galleryStore[CHANGE_GALLERY_DELETED_STATE]({ value: true });
+            next()
+          }
+        }
+      ]
+    }
   ],
 });
 
