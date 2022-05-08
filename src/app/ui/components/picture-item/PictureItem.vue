748f075776d4c1a0ba29870528302d560721d885
gcaso <76.giuseppe.caso@gmail.com>
2022-05-08 12:49:13 +0200

    improve image load

--- src/app/ui/components/picture-item/PictureItem.vue
+++ src/app/ui/components/picture-item/PictureItem.vue
@@ -3,6 +3,7 @@
     ref="root"
     :class="[
       isDisabled ? 'picture-item--is-disabled' : null,
+      hasLoading ? 'picture-item--is-loading' : null,
       'picture-item'
     ]"
     :data-index="id"
@@ -11,12 +12,14 @@
       data-image
       :data-src="src"
       loading="lazy"
+      @load="lazyLoadEnd"
     />
 
     <figcaption
       v-if="$slots['caption']"
       class="picture-item__caption"
     >
+      {{ hasLoading }}
       <slot name="caption" />
     </figcaption>
 
@@ -30,6 +33,9 @@
 </template>
   
 <script setup lang='ts'>
+  import { onMounted, ref, computed } from "vue";
+  import { Observe } from "@/app/helpers/utilities/observer";
+
   interface Props {
       id: string;
       src: string;
@@ -41,5 +47,29 @@
     src: null,
     isDisabled: false
   });
+
+  const root = ref(null)
+  const hasLoading = ref(true)
+
+  onMounted(() => {
+    const child = root.value.children[0];
+    if (!child) return false;
+
+    const obs = new Observe();
+    obs.create({
+      element: root.value,
+      action: async (): void => {
+        const { src } = child.dataset
+        child.src = src
+      }
+    })
+  })
+
+  const lazyLoadEnd = (e: any): void => {
+    const { target } = e
+    target.removeAttribute('data-src')
+    target.classList.add('is--load-end')
+    hasLoading.value = false
+  }
 </script>
 <style lang="scss" src="./PictureItem.scss" />
